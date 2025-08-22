4e253b5b053b0d626cbd80143f82a461447a7bc0
rehanulhb <rehanulh@gmail.com>
2025-08-22 23:31:18 +0200

    Added Joi Validation to student Controller

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -5,14 +5,128 @@ import Joi from 'joi';
 const createStudent = async (req: Request, res: Response) => {
   try {
     //Creating a schema validation using Joi
-    const JoivalidationSchema = Joi.object({
-      id: Joi.string(),
-      name: {
-        firstName: Joi.string().max(20),
-        middleName: Joi.string().max(20),
-        lastName: Joi.string().max(20),
-      },
-      gender: Joi.string().required().valid(['male', 'female', 'other']),
+    const userNameJoiSchema = Joi.object({
+      firstName: Joi.string()
+        .trim()
+        .max(20)
+        .required()
+        .custom((value, helpers) => {
+          const capitalized = value.charAt(0).toUpperCase() + value.slice(1);
+          if (value !== capitalized) {
+            return helpers.error('string.capitalize');
+          }
+          return value;
+        })
+        .messages({
+          'any.required': 'First Name is Required',
+          'string.max': 'First Name Cannot be more than 20 Characters',
+          'string.capitalize': '{{#label}} must start with a capital letter',
+        }),
+      middleName: Joi.string().allow('', null),
+      lastName: Joi.string()
+        .required()
+        .pattern(/^[A-Za-z]+$/)
+        .messages({
+          'any.required': 'Last Name is Required',
+          'string.pattern.base': '{#value} is not valid',
+        }),
+    });
+
+    // Guardian Schema
+    const guardianJoiSchema = Joi.object({
+      fatherName: Joi.string().required().messages({
+        'any.required': "Father's Name is required",
+      }),
+      fatherOccupation: Joi.string().required().messages({
+        'any.required': "Father's Occupation is required",
+      }),
+      fatherContactNo: Joi.string().required().messages({
+        'any.required': "Father's Contact Number is required",
+      }),
+      motherName: Joi.string().required().messages({
+        'any.required': "Mother's Name is required",
+      }),
+      motherOccupation: Joi.string().required().messages({
+        'any.required': "Mother's Occupation is required",
+      }),
+      motherContactNo: Joi.string().required().messages({
+        'any.required': "Mother's Contact Number is required",
+      }),
+    });
+
+    // Local Guardian Schema
+    const localGuardianJoiSchema = Joi.object({
+      name: Joi.string().required().messages({
+        'any.required': 'Local Guardian Name is required',
+      }),
+      occupation: Joi.string().required().messages({
+        'any.required': 'Local Guardian Occupation is required',
+      }),
+      contactNo: Joi.string().required().messages({
+        'any.required': 'Local Guardian Contact Number is required',
+      }),
+      address: Joi.string().required().messages({
+        'any.required': 'Local Guardian Address is required',
+      }),
+    });
+
+    // Student Schema
+    const studentJoiSchema = Joi.object({
+      id: Joi.string().required().messages({
+        'any.required': 'Student ID is required',
+      }),
+      name: userNameJoiSchema.required().messages({
+        'any.required': 'Student Name is required',
+      }),
+      gender: Joi.string()
+        .valid('male', 'female', 'other')
+        .required()
+        .messages({
+          'any.only':
+            "The Gender field can only be one of the following: 'male', 'female', or 'other'.",
+          'any.required': 'Gender is required',
+        }),
+      dateOfBirth: Joi.string().optional(),
+
+      email: Joi.string().email().required().messages({
+        'string.email': '{#value} is not a valid email type',
+        'any.required': 'Email is required',
+      }),
+
+      contactNo: Joi.string().required().messages({
+        'any.required': 'Contact Number is required',
+      }),
+      emergencyContactNo: Joi.string().required().messages({
+        'any.required': 'Emergency Contact Number is required',
+      }),
+      bloodGroup: Joi.string()
+        .valid('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')
+        .messages({
+          'any.only': '{#value} is not a valid blood group',
+        }),
+
+      presentAddress: Joi.string().required().messages({
+        'any.required': 'Present Address is required',
+      }),
+      permanentAddress: Joi.string().required().messages({
+        'any.required': 'Permanent Address is required',
+      }),
+
+      guardian: guardianJoiSchema.required().messages({
+        'any.required': 'Guardian information is required',
+      }),
+      localGuardian: localGuardianJoiSchema.required().messages({
+        'any.required': 'Local Guardian information is required',
+      }),
+
+      profileImg: Joi.string().uri().optional(),
+
+      isActive: Joi.string()
+        .valid('active', 'blocked')
+        .default('active')
+        .messages({
+          'any.only': "Status must be either 'active' or 'blocked'.",
+        }),
     });
 
     const { student: studentData } = req.body;
