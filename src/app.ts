e3115304fd4b498ab42e1ef15944aa24bd600e6a
rehanulhb <rehanulh@gmail.com>
2025-09-08 22:30:01 +0200

    Created Index Route and Module Summary - Done

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -12,8 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 //Applications Routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
