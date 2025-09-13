4866519869777ecb1470120d1fb282ea53dcac59
rehanulhb <rehanulh@gmail.com>
2025-09-13 21:48:16 +0200

    Working on Implementing ValidateRequest Middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,25 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { UserControllers } from './user.controller';
+import { ZodObject } from 'zod';
 
 const router = express.Router();
 
-const shenaBahini = (req: Request, res: Response, next: NextFunction) =>{
-    console.log(req.body);
+const validateRequest = (schema : ZodObject) => {
+return async(req: Request, res: Response, next: NextFunction) =>{
+    //Validation
+    const zodParsedData = await schema.parseAsync({
+        {
+            body: req.body;
+        }
+    });
+
+
+
+
     next();
 }
-
+};
 //Will Call controller Function
-router.post('/create-student',shenaBahini, UserControllers.createStudent);
+router.post('/create-student',validateRequest('validateRequest'), UserControllers.createStudent);
 
 export const UserRoutes = router;
