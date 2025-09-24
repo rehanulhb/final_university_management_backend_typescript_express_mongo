f2ac2cad8eba4de6e161b03aa68232375d693a41
rehanulhb <rehanulh@gmail.com>
2025-09-24 20:57:14 +0200

    Handle Logical Validation of Academic Semester

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,8 +1,19 @@
+import { academicSemesterNameCodeMapper } from "./academicSemester.constant";
 import { TAcademicSemester, TAcademicSemesterCode } from "./academicSemester.interface";
 import { AcademicSemester } from "./academicSemester.model";
 
 
 const createAcademicSemesterIntoDB = async(payload: TAcademicSemester) =>{
+
+    //Semester name --> Semester code
+
+    
+
+    
+    //
+    if(academicSemesterNameCodeMapper[payload.name]!==payload.code){
+        throw new Error('Invalid Semster Code')
+    }
 
     
     const result = await AcademicSemester.create(payload)
