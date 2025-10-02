99e0c5a9feac9483b375fa0e21d135fc390e7846
rehanulhb <rehanulh@gmail.com>
2025-10-02 23:18:07 +0200

    Implement logic to handle dynamically update non-primitive fields

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -29,9 +29,47 @@ const getSingleStudentFromDB = async (id: string) => {
 
 const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
 
+  const {name, guardian, localGuardian, ...remainingStudentData} = payload;
+
+  const modifiedUpdatedData : Record<string, unknown> = {...remainingStudentData};
+
+  /*
+  Guarding: {
+  fatherOccupation: 'Teacher'
+  }
+  guardian.fatherOccupation = teacher
+
+  name.firstname = 'Rehanul'
+  name.lastName = 'Bappy'
+
+  */
+
+
+  if(name && Object.keys(name).length){
+    for(const [key, value] of Object.entries(name)){
+      modifiedUpdatedData[`name.${key}`] = value;
+      
+    }
+  }
+
+  if(guardian && Object.keys(guardian).length){
+    for(const [key, value] of Object.entries(guardian)){
+      modifiedUpdatedData[`guardian.${key}`] = value;
+      
+    }
+  }
+  if(localGuardian && Object.keys(localGuardian).length){
+    for(const [key, value] of Object.entries(localGuardian)){
+      modifiedUpdatedData[`localGuardian.${key}`] = value;
+      
+    }
+  }
+
+  console.log(modifiedUpdatedData);
+
 
   // const result = await Student.findOne({ id });
-  const result = await Student.findOneAndUpdate({id}, payload);
+  const result = await Student.findOneAndUpdate({id}, modifiedUpdatedData, {new: true, runValidators: true});
   return result;
 };
 
@@ -60,6 +98,7 @@ const deleteStudentFromDB = async (id: string) => {
   }catch(err){
     await session.abortTransaction();
     await session.endSession();
+    throw new AppError(status.BAD_REQUEST, 'Failed to delete Student')
 
   }
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -61,6 +61,8 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
   catch(err){
     await session.abortTransaction();
     await session.endSession();
+
+    throw new AppError(status.BAD_REQUEST, 'Failed to create Student')
   }
 
   
