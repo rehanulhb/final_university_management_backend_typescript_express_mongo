6d227e236e54905fe63eff1b23c22a1dc8af7b8d
rehanulhb <rehanulh@gmail.com>
2025-10-06 00:55:39 +0200

    Pagination and Field Limiting

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -7,8 +7,10 @@ import status from 'http-status';
 
 const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
 
-  console.log('Base Query', query);
+  
   const queryObj = {...query};
+
+  
 
   //{email: {$regex: query.searchTerm, $options: i}}
   //{presentAddress: {$regex: query.searchTerm, $options: i}}
@@ -29,10 +31,11 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   })
 
   //Filtering 
-  const excludeFields = ['searchTerm', 'sort', 'limit']
+  const excludeFields = ['searchTerm', 'sort', 'limit', 'page']
 
   excludeFields.forEach((el) => delete queryObj[el]);
-  
+
+  console.log({query}, {queryObj});  
 
   const filterQuery = searchQuery.find(queryObj).populate('admissionSemester').populate({
     path: 'academicDepartment',
@@ -49,12 +52,22 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
 
   const sortQuery =  filterQuery.sort(sort);
 
+  let page = 1;
   let limit =1;
+  let skip =0;
+
   if(query.limit){
-    limit = query.limit;
+    limit = Number(query.limit);
   }
 
-  const limitQuery = await sortQuery.limit(limit);
+  if(query.page){
+    page = Number(query.page);
+    skip = (page-1)*limit;
+  }
+
+  const paginateQuery = sortQuery.skip(skip);
+
+  const limitQuery = await paginateQuery.limit(limit);
 
 
   return limitQuery;
