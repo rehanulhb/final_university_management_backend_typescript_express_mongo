c84356091261a51030b8df73496ae6b7e56d7284
rehanulhb <rehanulh@gmail.com>
2025-10-10 02:33:58 +0200

    Created Course Service , Controller, & Validation

---
+++ src/app/modules/Course/course.controller.ts
@@ -0,0 +1,82 @@
+
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+import status from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import { CourseServices } from './course.service';
+import sendResponse from '../../utils/sendResponse';
+
+
+const createCourse= catchAsync(async (req, res) => {
+
+    const result = await CourseServices.createCourseIntoDB(
+      req.body
+    );
+
+    sendResponse(res, {
+      statusCode: status.OK,
+      success: true,
+      message: 'Course is Created Successfully',
+      data: result,
+    });
+  
+})
+
+const getAllCourses = catchAsync(async (req, res) => {
+  const result = await CourseServices.getAllCoursesFromDB();
+
+  sendResponse(res, {
+      statusCode: status.OK,
+      success: true,
+      message: 'Courses are retrieved Successfully',
+      data: result,
+    });
+});
+
+const getSingleCourse = catchAsync(async(req, res)=>{
+  const {id} = req.params;
+  const result = await CourseServices.getSingleCourseFromDB(id!);
+
+  sendResponse(res, {
+      statusCode: status.OK,
+      success: true,
+      message: 'Course is retrieved Successfully',
+      data: result,
+    });
+
+})
+
+const deleteCourse = catchAsync(async(req, res)=>{
+  const {id} = req.params;
+  const result = await CourseServices.deleteCourseFromDB(id!);
+
+  sendResponse(res, {
+      statusCode: status.OK,
+      success: true,
+      message: 'Course is Deleted Successfully',
+      data: result,
+    });
+
+})
+
+
+
+// const updateAcademicFaculty = catchAsync(async(req, res)=>{
+//   const {facultyId} = req.params;
+//   const result = await AcademicFacultyServices.updateAcademicFacultyIntoDB(facultyId!, req.body,);
+
+//   sendResponse(res, {
+//       statusCode: status.OK,
+//       success: true,
+//       message: 'Academic Faculty is Updated Successfully',
+//       data: result,
+//     });
+
+// })
+
+export const CourseControllers = {
+  createCourse,
+  getAllCourses,
+  getSingleCourse,
+  deleteCourse,
+  
+};
