873ffc516ed6a7a8f5eae7ac01bd1fb9aceabb7b
rehanulhb <rehanulh@gmail.com>
2025-10-30 03:04:50 +0100

    EnrolledCourse Interface, Controller, route, model, etc Created

---
+++ src/app/modules/EnrolledCourse/enrolledCourse.route.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { EnrolledCourseControllers } from './enrolledCourse.controller';
+import { EnrolledCourseValidations } from './enrolledCourse.validation';
+import { USER_ROLE } from '../user/user.constant';
+
+const router = express.Router();
+
+router.post(
+  '/create-enrolled-course',
+  auth(USER_ROLE.student),
+  validateRequest(
+    EnrolledCourseValidations.createEnrolledCourseValidationZodSchema,
+  ),
+  EnrolledCourseControllers.createEnrolledCourse,
+);
+
+// router.patch(
+//   '/update-enrolled-course-marks',
+//   auth('faculty'),
+//   validateRequest(
+//     EnrolledCourseValidations.updateEnrolledCourseMarksValidationZodSchema,
+//   ),
+//   EnrolledCourseControllers.updateEnrolledCourseMarks,
+// );
+
+export const EnrolledCourseRoutes = router;
